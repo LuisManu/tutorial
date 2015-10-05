4f548b6720c5abc91f487e24361d04bd49a4cb58
Luis Gutierrez <luismanugutierrez@gmail.com>
2015-10-04 20:45:04 -0700

    Django ReST 1 - Serialization
    JSON response

--- snippets/models.py
+++ snippets/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from pygments.lexers impoer get_all_lexers
+from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
 
@@ -13,5 +13,12 @@ STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 class Snippet(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	title = models.CharField(max_length=100, blank=True, default='')
+	code = models.TextField()
+	linenos = models.BooleanField(default=False)
+	language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+	style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+
+	class Meta:
+		ordering = ('created',)
 
 
